0ab1838736cf1f464f260ac9cff349868c705b2f
Anish <anish.tr@clariontechnologies.co.in>
2015-08-18 19:26:29 +0530

    asdsad

--- js/my-app.js
+++ js/my-app.js
@@ -7,22 +7,10 @@ var mainView = myApp.addView('.view-main', {
     domCache: true
 });
 $$('.ac-5').on('click', function() {
-    var buttons = [{
-        text: 'Button1',
-        onClick: function() {
-            myApp.alert('Button1 clicked');
-        }
-    }, {
-        text: 'Button2',
-        onClick: function() {
-            myApp.alert('Button2 clicked');
-        }
-    }, {
-        text: 'Cancel',
-        color: 'red',
-        onClick: function() {
-            myApp.alert('Cancel clicked');
-        }
-    }, ];
-    myApp.actions(buttons);
+     // Check first, if we already have opened picker
+  if ($$('.picker-modal.modal-in').length > 0) {
+    myApp.closeModal('.picker-modal.modal-in');
+  }
+    myApp.pickerModal('.picker-info')
+  
 });
